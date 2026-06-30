d3f8325fcb8cbb1361b6738d55568b653a5883a8
Thomas CASTELLY <thomas@castelly.fr>
2026-06-30 10:23:12 +0200

    upgrade to TS with ncc builder

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { setFailed, setOutput } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
+
+type Octokit = InstanceType<typeof GitHub>;
+
+const getPackageJson = async (ref: string, octokit: Octokit) => {
+  const packageJSON = (await octokit.rest.repos.getContent({
+    ...context.repo,
+    path: process.env['INPUT_PATH'] || 'package.json',
+    ref,
+  }));
+
+  const packageJSONData = packageJSON.data as { content: string };
+
+  if (!packageJSONData) {
+    throw new Error(`Could not find package.json for commit ${ref}`);
+  }
+  return JSON.parse(Buffer.from(packageJSONData.content, 'base64').toString());
+};
+
+const run = async () => {
+  const token = process.env['GITHUB_TOKEN'];
+  if (!token) {
+    throw new Error('GITHUB_TOKEN not provided');
+  }
+
+  const octokit = getOctokit(token);
+  const currentRef = context.sha;
+  const previousRef = ((await octokit.rest.repos.getCommit({
+    ...context.repo,
+    ref: currentRef,
+  })).data.parents[0] || {}).sha;
+
+  const currentPackageJSON = await getPackageJson(currentRef, octokit);
+  setOutput('current-package-version', currentPackageJSON.version);
+
+  if (!previousRef) {
+    setOutput('has-updated', true);
+    return;
+  }
+
+  const previousPackageJSON = await getPackageJson(previousRef, octokit);
+  setOutput('has-updated', currentPackageJSON.version !== previousPackageJSON.version);
+};
+
+run().catch(error => {
+  setFailed(error.message);
+});
